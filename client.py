6f7ae58bb0362d97258784b53be5561e14bf6dc8
David Stuebe <stu3b3@gmail.com>
2019-04-13 15:51:13 -0400

    cleanup a bit

--- client.py
+++ client.py
@@ -1,15 +1,14 @@
 import cv2
 import numpy as np
 import socket
-import sys
-import pickle
 
 cap=cv2.VideoCapture(0)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 clientsocket.connect(('localhost',8089))
+
+# TODO add zlib compression - requires variable size messages
 while True:
     ret,frame=cap.read()
-    #clientsocket.sendall(struct.pack("H", len(data))+data) ### new code
-    ### Fixed size 2764800
+    ### Assumes fixed size 2764800 bytes, (720, 1280, 3)
     clientsocket.sendall(frame.tobytes())
     print("Sent frame!!!")
--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 import socket
-import sys
 import cv2
 import numpy as np
-import ipdb
 import io
 
 HOST=''
@@ -18,7 +16,8 @@ print('Socket now listening')
 
 conn,addr=s.accept()
 
-MSGLEN = 2764800
+MSGLEN = 2764800 # assumes (720, 1280, 3)
+# TODO add zlib compression - requires variable length frames...
 
 with io.BytesIO(bytearray(MSGLEN)) as mybuffer:
 
@@ -35,22 +34,3 @@ with io.BytesIO(bytearray(MSGLEN)) as mybuffer:
         print("Got frame!!! {}".format(frame))
         cv2.imshow('frame',frame)
         cv2.waitKey(1)
-
-# ### new
-# data = b""
-# payload_size = struct.calcsize("H") 
-# while True:
-#     while len(data) < payload_size:
-#         data += conn.recv(4096)
-#     packed_msg_size = data[:payload_size]
-#     data = data[payload_size:]
-#     msg_size = struct.unpack("H", packed_msg_size)[0]
-#     while len(data) < msg_size:
-#         data += conn.recv(4096)
-#     frame_data = data[:msg_size]
-#     data = data[msg_size:]
-#     ###
-
-#     frame=pickle.loads(frame_data)
-#     print(frame)
-#     cv2.imshow('frame',frame)
